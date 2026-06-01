e1d267df6d00a5871c09c027b9567c54cfbf5d48
Yu-Chieh Hsin <hsin-oshirogreymac@Yu-Chiehs-MacBook-Air-2.local>
2026-06-01 16:43:26 -0700

    Finished part two of UI part of delay plugin, added in visibile sliders in the GUI. Will add some kind of design to sliders later

--- delay/PluginEditor.cpp
+++ delay/PluginEditor.cpp
@@ -15,6 +15,50 @@ DelayPlugin2AudioProcessorEditor::DelayPlugin2AudioProcessorEditor (DelayPlugin2
 {
     // Make sure that before the constructor has finished, you've set the
     // editor's size to whatever you need it to be.
+    auto& params = processor.getParameters();
+
+    juce::AudioParameterFloat* dryWetParameter = (juce::AudioParameterFloat*)params.getUnchecked(0);
+    
+    mDryWetSlider.setBounds(0, 0, 100, 100);
+    mDryWetSlider.setSliderStyle(juce::Slider::SliderStyle::RotaryVerticalDrag);
+    mDryWetSlider.setTextBoxStyle(juce::Slider::TextEntryBoxPosition::NoTextBox, true, 0, 0);
+    mDryWetSlider.setRange(dryWetParameter->range.start, dryWetParameter->range.end);
+    mDryWetSlider.setValue(*dryWetParameter);
+    addAndMakeVisible(mDryWetSlider);
+    mDryWetSlider.onDragStart = [dryWetParameter] {dryWetParameter->beginChangeGesture();};
+    mDryWetSlider.onValueChange = [this, dryWetParameter] { *dryWetParameter = mDryWetSlider.getValue();};
+    mDryWetSlider.onDragEnd = [dryWetParameter] {dryWetParameter->endChangeGesture();};
+    //size
+    //style
+    //text box
+    //range
+    //value
+    //visibility
+
+    juce::AudioParameterFloat* FeedbackParameter = (juce::AudioParameterFloat*)params.getUnchecked(1);
+    mFeedbackSlider.setBounds(100, 0, 100, 100);
+    mFeedbackSlider.setSliderStyle(juce::Slider::SliderStyle::RotaryVerticalDrag);
+    mFeedbackSlider.setTextBoxStyle(juce::Slider::TextEntryBoxPosition::NoTextBox, true, 0, 0);
+    mFeedbackSlider.setRange(FeedbackParameter->range.start, FeedbackParameter->range.end);
+    mFeedbackSlider.setValue(*FeedbackParameter);
+    addAndMakeVisible(mFeedbackSlider);
+    mFeedbackSlider.onDragStart = [FeedbackParameter] {FeedbackParameter->beginChangeGesture();};
+    mFeedbackSlider.onValueChange = [this, FeedbackParameter] { *FeedbackParameter = mFeedbackSlider.getValue();};
+    mFeedbackSlider.onDragEnd = [FeedbackParameter] {FeedbackParameter->endChangeGesture();};
+    //feedback slider
+    
+    juce::AudioParameterFloat* DelayTimeParameter = (juce::AudioParameterFloat*)params.getUnchecked(2);
+    mDelayTimeSlider.setBounds(200, 0, 100, 100);
+    mDelayTimeSlider.setSliderStyle(juce::Slider::SliderStyle::RotaryVerticalDrag);
+    mDelayTimeSlider.setTextBoxStyle(juce::Slider::TextEntryBoxPosition::NoTextBox, true, 0, 0);
+    mDelayTimeSlider.setRange(DelayTimeParameter->range.start, DelayTimeParameter->range.end);
+    mDelayTimeSlider.setValue(*DelayTimeParameter);
+    addAndMakeVisible(mDelayTimeSlider);
+    mDelayTimeSlider.onDragStart = [DelayTimeParameter] {DelayTimeParameter->beginChangeGesture();};
+    mDelayTimeSlider.onValueChange = [this, DelayTimeParameter] { *DelayTimeParameter = mDelayTimeSlider.getValue();};
+    mDelayTimeSlider.onDragEnd = [DelayTimeParameter] {DelayTimeParameter->endChangeGesture();};
+    //delaytime slider
+    
     setSize (400, 300);
 }
 
